69b6c22d25a8a15841a5140106a2b4f17bddf498
Francisc0Ramos <105827951+Francisc0Ramos@users.noreply.github.com>
2022-12-14 01:03:44 -0600

    arreglo de errores

--- App/src/main/java/Visual/interfaces/PanelCliente.java
+++ App/src/main/java/Visual/interfaces/PanelCliente.java
@@ -47,7 +47,7 @@ public class PanelCliente extends javax.swing.JPanel {
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+ void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
         nombreC = new javax.swing.JLabel();
